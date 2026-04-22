587816aaa900dacd6fe7e5b98c9fd3a235d30e1e
Mariana Guerrero <160988250+KeeperMitch@users.noreply.github.com>
2026-04-22 12:49:51 -0600

    Update MOD4X7SEG with enable output and comments
    Removed author name and added enable output for segment control. Commented out the bin_to_7seg_converter module.

--- src/design/MOD4X7SEG.sv
+++ src/design/MOD4X7SEG.sv
@@ -1,10 +1,12 @@
 // MODULO 5.2 - Seven Segment Display
-// Autor: Andres Obregon Lopez
+// Autor: 
 // Circuito combinacional para el display de 7 segmentos en representacion hexadecimal (0-F)
 
 module top (
     input logic [3:0] sw,           // Bits de entrada: AIN ,BIN ,CIN ,DIN (MSB a LSB)
     output logic [6:0] segments     // Segments: SA, SB, SC, SD, SE, SF, SG (LSB a MSB)
+    output logic [3:0] enable // Habilita la pocisiòn de cada segmento
+
 );
 
 
@@ -72,3 +74,34 @@ assign segments[5] = SF;
 assign segments[6] = SG;
 
 endmodule
+
+/*
+module bin_to_7seg_converter (
+    input  logic [3:0] data_in,        // Entrada de 4 bits (datos corregidos o posición de error)
+    output logic [6:0] segments        // Salida para el display de 7 segmentos
+);
+
+    always_comb begin
+        case (data_in)
+            4'b0000: segments = 7'b0111111; // 0
+            4'b0001: segments = 7'b0000110; // 1
+            4'b0010: segments = 7'b1011011; // 2
+            4'b0011: segments = 7'b1001111; // 3
+            4'b0100: segments = 7'b1100110; // 4
+            4'b0101: segments = 7'b1101101; // 5
+            4'b0110: segments = 7'b1111101; // 6
+            4'b0111: segments = 7'b0000111; // 7
+            4'b1000: segments = 7'b1111111; // 8
+            4'b1001: segments = 7'b1101111; // 9
+            4'b1010: segments = 7'b1110111; // A
+            4'b1011: segments = 7'b1111100; // B
+            4'b1100: segments = 7'b0111001; // C
+            4'b1101: segments = 7'b1011110; // D
+            4'b1110: segments = 7'b1111001; // E
+            4'b1111: segments = 7'b1110001; // F
+            default: segments = 7'b0111111; // Apagar
+        endcase
+    end
+endmodule
+
+*/
